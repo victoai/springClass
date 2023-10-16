37d55125d339491f7d6cd4726e5342e1301859f2
jooyeon <victoai999@gmail.com>
2023-10-16 17:50:46 +0900

    스프링7일차

---
+++ batisPrj2/src/main/java/com/acorn/batis2/Member.java
@@ -0,0 +1,12 @@
+package com.acorn.batis2;
+
+import lombok.Data;
+
+@Data
+public class Member {
+	
+	String id;
+	String pwd;
+	String name;
+
+}
---
+++ day7intefacePrj/src/main/java/com/acorn/service/MyService.java
@@ -0,0 +1,26 @@
+package com.acorn.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service 	   
+public class MyService {
+
+	@Autowired
+	MyRepository repository;
+	
+	
+	@Autowired
+	GoogleApi api;
+        
+	//Member m
+	public void userRegister( String id, String name ) {
+		// 
+		//회원가입
+		//위도, 경도String latitude , String longitude
+		double[] result  =api.위도경도구하기();
+		repository.insertMember(id, name, String.valueOf(result[0]), String.valueOf(result[1]));		
+		
+	}
+	
+}
